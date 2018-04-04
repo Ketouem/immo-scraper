fe9e5ac22e01bfd37836279d7e563fc48f965e4a
Ketouem <ketouem@gmail.com>
2018-04-04 21:12:10 +0200

    Serialize results into proper structs

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "flag"
   "fmt"
+  "strconv"
   "strings"
   "os"
 
@@ -14,9 +15,30 @@ var (
   leboncoinStartUrl string
 )
 
+type Result struct {
+  Title string
+  Price int
+  Link string
+  // LivingSpace int
+  // Rooms int
+}
+
+func (r *Result) Display() {
+  fmt.Println("Titre:", r.Title)
+  fmt.Println("Prix:", r.Price)
+  fmt.Println("Lien:", r.Link)
+  // fmt.Println("Surface (m2):", r.LivingSpace)
+  // fmt.Println("Pièces:", r.Rooms)
+}
+
 func init() {
   flag.StringVar(&leboncoinStartUrl, "leboncoin-start-url", "", "www.leboncoin.fr start url")
   flag.Parse()
+
+  formatter := &logrus.TextFormatter{
+    FullTimestamp: true,
+  }
+  logrus.SetFormatter(formatter)
 
   if len(leboncoinStartUrl) == 0 {
     logrus.Fatal("leboncoin-start-url must be provided and not empty")
@@ -25,23 +47,48 @@ func init() {
 }
 
 func main() {
+  links := gatherLeBonCoinLinks(leboncoinStartUrl)
+  extractResults(links)
+}
+
+func extractResults(links []string) (results []Result) {
+  logrus.WithFields(logrus.Fields{
+    "number": len(links),
+  }).Info("Extracting results from links")
+  c := colly.NewCollector(
+    colly.AllowedDomains("www.leboncoin.fr"),
+  )
+
+  c.OnHTML("#container", func(e *colly.HTMLElement) {
+    rawPrice := strings.Split(e.ChildText("div[data-qa-id=adview_price]"), "€")[0]
+    price, _ := strconv.Atoi(strings.Replace(rawPrice, " ", "", -1))
+    title := e.ChildText("div[data-qa-id='adview_title']")
+    link := "https://" + e.Request.URL.Host + e.Request.URL.Path
+    result := Result {
+      title,
+      price,
+      link,
+    }
+    results = append(results, result)
+  })
+
+  for _, link := range links {
+    c.Visit(link)
+  }
+  logrus.WithFields(logrus.Fields{
+    "number": len(results),
+  }).Info("Results extracted from links ✅")
+  return
+}
+
+func gatherLeBonCoinLinks(searchUrl string) (links []string){
+  logrus.Info("Gathering leboncoin links")
   c := colly.NewCollector(
     colly.AllowedDomains("www.leboncoin.fr"),
   )
 
   c.OnHTML("li[itemscope] a", func(e *colly.HTMLElement) {
-    fmt.Println("Description:", e.ChildText("h2[class='item_title']"))
-    fmt.Println("Prix:", e.ChildAttr("h3[class=item_price]", "content"))
-    classifiedHref := e.Attr("href")
-    queryStringStart := strings.Index(classifiedHref, "?")
-    cleansedHref := ""
-    if queryStringStart > -1 {
-      cleansedHref = classifiedHref[0:queryStringStart]
-    } else {
-      cleansedHref = classifiedHref
-    }
-    fmt.Println("Lien:", e.Request.AbsoluteURL(cleansedHref))
-    fmt.Println()
+    links = append(links, e.Attr("href"))
   })
 
   c.OnHTML("#listingAds div.pagination_links_container", func(e *colly.HTMLElement) {
@@ -49,9 +96,6 @@ func main() {
     c.Visit(e.Request.AbsoluteURL(nextPageLink))
   })
 
-  c.OnRequest(func(r *colly.Request) {
-    fmt.Println("Visiting", r.URL)
-  })
-
-  c.Visit(leboncoinStartUrl)
+  c.Visit(searchUrl)
+  return
 }
